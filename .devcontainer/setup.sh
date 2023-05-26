25720df37f73859a0fecfdb228a6be40225f3511
Jean Honlet <jehon@users.noreply.github.com>
2023-05-26 09:32:00 +0200

    build: clean up setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,27 +4,21 @@ set -o errexit
 set -o pipefail
 
 SWD="$(dirname "${BASH_SOURCE[0]}")"
-
 PRJ_ROOT="$(dirname "$SWD")"
-export PRJ_ROOT
-
-export DEBIAN_FRONTEND=noninteractive
 
 apt_install() {
     # /repo is not always available
-    apt install --quiet --yes "$@"
+    DEBIAN_FRONTEND=noninteractive apt install --quiet --yes "$@"
 }
 
 apt update
 
-apt_install curl git \
-    python3 python3-pip python3-netaddr python3-passlib python3-apt \
-    default-libmysqlclient-dev
+apt_install \
+    curl git \
+    python3 python3-pip \
+    python3-netaddr python3-passlib python3-apt default-libmysqlclient-dev 
 
-#    libmariadb-dev
-
-mkdir -p tmp
-chmod 777 tmp
+mkdir --mode=0777 tmp
 curl -fsSL https://jehon.github.io/packages/jehon.deb -o tmp/jehon.deb
 apt_install ./tmp/jehon.deb
 
