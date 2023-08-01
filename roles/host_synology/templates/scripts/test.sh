d618f08dc0368566827e48479fee9328f64eb07f
Jean Honlet <jehon@users.noreply.github.com>
2023-08-01 21:00:50 +0200

    style: enhance test.sh

--- roles/host_synology/templates/scripts/test.sh
+++ roles/host_synology/templates/scripts/test.sh
@@ -5,4 +5,9 @@ set -o errexit
 # shellcheck source-path=SCRIPTDIR
 . "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/lib.sh
 
-"$SCRIPTS_FOLDER"/daily.d/80-photo-gallery.sh
+date
+
+time "$SCRIPTS_FOLDER"/daily.d/80-photo-gallery.sh
+
+echo "Test script ended with success"
+date
