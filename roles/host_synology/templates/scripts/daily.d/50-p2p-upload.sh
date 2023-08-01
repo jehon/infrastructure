2e9872450f556ade8c5ae5c0a8d49d47580458a7
Jean Honlet <jehon@users.noreply.github.com>
2023-08-01 21:12:37 +0200

    fix: watch folder

--- roles/host_synology/templates/scripts/daily.d/50-p2p-upload.sh
+++ roles/host_synology/templates/scripts/daily.d/50-p2p-upload.sh
@@ -6,7 +6,7 @@ set -o pipefail
 # shellcheck source-path=SCRIPTDIR/../
 . "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../lib.sh
 
-CLOUD_WATCH="cloud:/Workspaces/Jean/p2p/watch/"
+CLOUD_WATCH="cloud:/Workspaces/Jean/p2p/Watch/"
 SYNO_WATCH="$MAIN_VOLUME/p2p/watch/"
 
 SYNO_UPLOADED="$MAIN_VOLUME/p2p/downloaded"
