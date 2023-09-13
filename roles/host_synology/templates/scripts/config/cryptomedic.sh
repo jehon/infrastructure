71f5c471a7831b90d4eb5b1aa5c849e1c28a086a
Jean Honlet <jehon@users.noreply.github.com>
2023-09-13 06:25:28 +0200

    fix: parameters on generate

--- roles/host_synology/templates/scripts/config/cryptomedic.sh
+++ roles/host_synology/templates/scripts/config/cryptomedic.sh
@@ -4,13 +4,13 @@
 
 set -o errexit
 
-export CRYPTOMEDIC_DEPLOY_WEB_HOST="{{jehon.credentials.cryptomedic.http_host}}"
-export CRYPTOMEDIC_DEPLOY_WEB_PORT="{{jehon.credentials.cryptomedic.http_port}}"
-export CRYPTOMEDIC_DEPLOY_WEB_TOKEN="{{jehon.credentials.cryptomedic.http_token}}"
+export CRYPTOMEDIC_DEPLOY_WEB_HOST="{{jehon.credentials.cryptomedic.deploy_web_host}}"
+export CRYPTOMEDIC_DEPLOY_WEB_PORT="{{jehon.credentials.cryptomedic.deploy_web_port}}"
+export CRYPTOMEDIC_DEPLOY_WEB_TOKEN="{{jehon.credentials.cryptomedic.deploy_web_token}}"
 
-export CRYPTOMEDIC_DEPLOY_FILES_HOST="{{jehon.credentials.cryptomedic.deploy_host}}"
-export CRYPTOMEDIC_DEPLOY_FILES_USER="{{jehon.credentials.cryptomedic.deploy_user}}"
-export CRYPTOMEDIC_DEPLOY_FILES_PASSWORD="{{jehon.credentials.cryptomedic.deploy_password}}"
+export CRYPTOMEDIC_DEPLOY_FILES_HOST="{{jehon.credentials.cryptomedic.deploy_files_host}}"
+export CRYPTOMEDIC_DEPLOY_FILES_USER="{{jehon.credentials.cryptomedic.deploy_files_user}}"
+export CRYPTOMEDIC_DEPLOY_FILES_PASSWORD="{{jehon.credentials.cryptomedic.deploy_files_password}}"
 
 if [ -n "$1" ]; then
     "$@"
