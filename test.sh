57a37dc1e240717418c51f970d876c3696b4ba57
Jean Honlet <jehon@users.noreply.github.com>
2024-01-03 15:23:37 +0100

    refactor: use flow

--- test.sh
+++ test.sh
@@ -1,10 +1,17 @@
-set -o errexit
+#!/usr/bin/env bash
 
-./bin/jh-infrastructure setup dev
+clear
+
+set -o errexit
+set -o pipefail
+
+if [ -z "$1" ]; then
+    ./bin/jh-infrastructure setup dev
+fi
 
 cat <<EOS | ssh root@localhost
 
-systemctl restart grafana-agent.service || true
-journalctl -f -u grafana-agent.service
+systemctl restart grafana-agent-flow.service || true
+journalctl -xfu grafana-agent-flow.service
 
 EOS
