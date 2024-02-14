18c4a308bb416a901640065f60b3fbd03e91d3d7
Jean Honlet <jehon@users.noreply.github.com>
2024-02-14 07:52:52 +0100

    ci: fix setup and sudo

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,14 +6,24 @@ set -o pipefail
 SWD="$(dirname "$( realpath "${BASH_SOURCE[0]}")")"
 PRJ_ROOT="$(dirname "$SWD")"
 
+root_or_sudo() {
+    if [ $( id -u ) == 0 ]; then
+        # In Docker
+        "$@"
+    else
+        # On system
+        sudo "$@"
+    fi  
+}
+
 cd "${SWD}"
 
 apt_install() {
     # /repo is not always available
-    sudo DEBIAN_FRONTEND=noninteractive apt install --quiet --yes "$@"
+    root_or_sudo apt install --quiet --yes "$@"
 }
 
-sudo apt update
+root_or_sudo apt update
 
 apt_install \
     curl git sshpass \
@@ -24,10 +34,10 @@ mkdir --mode=0777 -p tmp
 curl -fsSL https://jehon.github.io/packages/jehon.deb -o tmp/jehon.deb
 apt_install ./tmp/jehon.deb
 
-sudo apt update
+root_or_sudo apt update
 
 if type direnv &>/dev/null ; then
     direnv allow "$PRJ_ROOT"/
 fi
 
-sudo /usr/sbin/jh-install-shellcheck
+root_or_sudo /usr/sbin/jh-install-shellcheck
