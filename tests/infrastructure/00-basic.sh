5b258f87645b7fc7e5330998278becf3fef095b4
Jean Honlet <jehon@users.noreply.github.com>
2024-02-26 12:07:57 +0100

    refactor: push all built in tmp

--- tests/infrastructure/00-basic.sh
+++ tests/infrastructure/00-basic.sh
@@ -26,7 +26,7 @@ set -o pipefail
 		cat inventory/50-hosts.yml
 		! cat inventory/50-hosts.yml | grep -q '!vault'
 
-		cat built/encryption-key
-		cat built/encryption-key | grep "encryption-key-mock"
+		cat tmp/infrasctructure/encryption-key
+		cat tmp/infrasctructure/encryption-key | grep "encryption-key-mock"
 	EOS
 ) | test_in_docker
