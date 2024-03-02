e35c359343f100851a1b7c83203b3b6e0c23c244
Jean Honlet <jehon@users.noreply.github.com>
2024-03-02 13:38:41 +0100

    chore: test script

--- test.sh
+++ test.sh
@@ -6,12 +6,17 @@ set -o errexit
 set -o pipefail
 
 if [ -z "$1" ]; then
-    ./bin/jh-infrastructure setup dev
+#    ./bin/jh-infrastructure setup dev
+    ./packages-deploy-patch.sh dev packages/jehon/usr/lib/systemd/system/mnt-hardware.mount
 fi
 
 cat <<EOS | ssh root@localhost
 
-systemctl restart grafana-agent.service || true
-journalctl -xfu grafana-agent.service
+systemctl daemon-reload
+systemctl restart mnt-hardware.mount
+sleep 1
+systemctl status mnt-hardware.mount
 
 EOS
+
+ls /mnt/hardware
