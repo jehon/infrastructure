1496880275f33e3f9cde3150385bf71311c906f8
Jean Honlet <jehon@users.noreply.github.com>
2024-04-09 11:46:19 +0200

    refactor: lib script helpers for fo

--- tests/scripts/lib-scripts-helpers.sh
+++ tests/scripts/lib-scripts-helpers.sh
@@ -4,11 +4,13 @@ set -o errexit
 
 TEST_SELF="$( realpath "$0" )"
 
-JH_TEST_NAME="$( basename "$( dirname "${TEST_SELF}" )" )/$( basename "${TEST_SELF}" )"
+if [ -z "$JH_TEST_NAME" ]; then
+    JH_TEST_NAME="$( basename "$( dirname "${TEST_SELF}" )" )/$( basename "${TEST_SELF}" )"
+fi
 export JH_TEST_NAME
 
 # shellcheck source-path=SCRIPTDIR
 . "$(dirname "${BASH_SOURCE[0]}")"/../test-helpers.sh
 
-export JH_TEST_SCRIPTS_TMP="${JH_TEST_TMP}/scripts"
+export JH_TEST_SCRIPTS_TMP="${JH_TEST_TMP}"
 mkdir -p "${JH_TEST_SCRIPTS_TMP}"
