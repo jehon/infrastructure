62ce1906336df25cb556b33cdae734c3110628fa
Jean Honlet <jehon@users.noreply.github.com>
2024-04-12 14:24:09 +0200

    chore: remove hyperv setup

--- packages/jehon/usr/sbin/jh-deb-helpers.sh
+++ packages/jehon/usr/sbin/jh-deb-helpers.sh
@@ -212,7 +212,7 @@ jh_deb_helper_ufw() {
 		NETWORKS="192.168.0.0/16"
 		;;
 	*)
-		echo "jh_deb_helper_ufw need a network explicitely [3]: public / hyperv / home" >&2
+		echo "jh_deb_helper_ufw need a network explicitely [3]" >&2
 		exit 1
 		;;
 	esac
