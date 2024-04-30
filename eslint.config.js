876e35f7c8d72f5c08e8a70445b26bc0a0e481e1
Jean Honlet <jehon@users.noreply.github.com>
2024-04-30 03:32:06 +0200

    build: use eslint flat config

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+const config = [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node
+      }
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "no-console": [
+        "error",
+        {
+          allow: ["warn", "error", "info", "assert"]
+        }
+      ],
+      "require-await": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_"
+        }
+      ]
+    }
+  },
+  // https://github.com/eslint/eslint/issues/17400#issuecomment-1646873272
+  // https://eslint.org/docs/latest/use/configure/configuration-files#globally-ignoring-files-with-ignores
+  {
+    ignores: ["**/data/**/*", "**/tmp/**"]
+  }
+];
+
+export default config;
