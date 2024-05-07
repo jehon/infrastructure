8c8d13ca0ff900ed35690833cc0fd5f77c5de0c3
Jean Honlet <jehon@users.noreply.github.com>
2024-05-07 14:54:25 +0200

    build: change dest of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const __dirname = import.meta.dirname;
 const config = {
   entry: "./node/src/fo.ts",
   output: {
-    path: path.join(__dirname, "packages/jehon/usr/bin"),
+    path: path.join(__dirname, "tmp/node/built"),
     filename: "jh-fo"
   },
   mode: "development",
