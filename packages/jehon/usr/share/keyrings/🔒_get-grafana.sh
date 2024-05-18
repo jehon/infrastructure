fd739295cc4d39bf233bfc6797c79028a23b0b1f
Jean Honlet <jehon@users.noreply.github.com>
2024-05-18 18:17:33 +0200

    chore: remove more grafana

--- packages/jehon/usr/share/keyrings/🔒_get-grafana.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-set -o errexit
-
-# shellcheck source-path=SCRIPTDIR/../../bin/
-. jh-lib
-
-curl -fsSL https://apt.grafana.com/gpg.key | gpg --dearmor --yes -o "$JH_SWD/jehon-grafana.gpg"
