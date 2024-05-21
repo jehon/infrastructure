f187d351e518dd11a689573f3e584496d49b6b7e
Jean Honlet <jehon@users.noreply.github.com>
2024-05-21 09:32:34 +0200

    feat: backup vps snapshots

---
+++ conf/auto/daily/backup-vps.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+shopt -s nullglob
+
+_SD="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+# shellcheck source-path=SCRIPTDIR/
+. "${_SD}/../../../bin/lib.sh"
+
+target="${JH_CLOUD_USER}/Systèmes/vps"
+
+rsync --recursive --itemize-changes \
+    vps:/var/backups/snapshot/ "${target}/snapshots"
