0fe94f913408eaaf02164c28d69995bcb9c0ab00
Jean Honlet <jehon@users.noreply.github.com>
2024-05-21 09:31:25 +0200

    refactor: use common variable 2

--- bin/wsl.sh
+++ bin/wsl.sh
@@ -5,7 +5,7 @@ set -o errexit
 # shellcheck source-path=SCRIPTDIR/
 . "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/lib.sh"
 
-if ! jh-fs not-empty pCloudDrive ; then
+if ! jh-fs not-empty "${JH_CLOUD_USER}"; then
     if [ -x ~/pcloud ]; then
         echo "Launching pcloud"
         JH_BACKGROUND_LOG=~/pcloud.wsl.log jh_background_process ~/pcloud
@@ -14,7 +14,7 @@ if ! jh-fs not-empty pCloudDrive ; then
     fi
 fi
 
-while true ; do
+while true; do
     jh-display-no-clear /usr/bin/jh-checks
     sleep 15s
 done
