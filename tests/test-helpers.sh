b2d224e32cb5e443cea2542e92c3e71d101f827a
Jean Honlet <jehon@users.noreply.github.com>
2024-05-22 08:04:33 +0200

    fix: some redirect

--- tests/test-helpers.sh
+++ tests/test-helpers.sh
@@ -34,7 +34,7 @@ log_failure() {
         fi
         echo -e "\e[1;31m\xE2\x9C\x98\e[1;00m Test '\e[1;33m$1\e[00m' failure: \e[1;31m$2\e[1;00m"
         echo "To have potentially more details, please run tests.sh with DEBUG=1"
-    ) >&3
+    ) >&2
     exit 1
 }
 
