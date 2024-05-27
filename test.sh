071accd513e4bde5f1ff03d798003555f6be5482
Jean Honlet <jehon@users.noreply.github.com>
2024-05-27 10:05:28 +0200

    fix: jehon-cockpit

--- test.sh
+++ test.sh
@@ -10,23 +10,27 @@ make tmp/node/built/.built
 target=kiosk
 service=getty@tty1.service
 
-./deploy-patch-from-packages "${target}" packages/jehon/usr/bin/jh-fo
-./deploy-patch-from-packages "${target}" packages/jehon-service-kiosk/usr/lib/systemd/system/jehon-kiosk.service
+# ./deploy-patch-from-packages "${target}" packages/jehon/usr/bin/jh-fo
+# ./deploy-patch-from-packages "${target}" packages/jehon-service-kiosk/usr/lib/systemd/system/jehon-kiosk.service
 ./deploy-patch-from-packages "${target}" packages/jehon-service-kiosk/usr/lib/systemd/system/getty@tty1.service.d/jehon-kiosk-override.conf
 scp tmp/node/built/jh-fo.cjs "${target}":/usr/share/jehon/node
 
+# shellcheck disable=SC2087 # client side expension
 ssh "${target}" <<EOS
 clear
 systemctl daemon-reload
 
 echo "Service: ${service}"
 
-echo "..."; sleep 1s
+echo "..."; sleep 1
 systemctl restart "${service}"
 
-echo "..."; sleep 5s
+echo "..."; sleep 5
 journalctl -u "${service}" -n 60
 
-echo "..."; sleep 5s
+echo "..."; sleep 5
 systemctl status "${service}"
+
+echo "..."; sleep 5
+ps -e | grep fbi
 EOS
