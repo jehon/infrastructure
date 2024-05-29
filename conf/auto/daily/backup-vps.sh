3b6436cad852f74dd55d29c54f7f083685031f7b
Jean Honlet <jehon@users.noreply.github.com>
2024-05-29 15:36:10 +0200

    fix: backup server

--- conf/auto/daily/backup-vps.sh
+++ conf/auto/daily/backup-vps.sh
@@ -11,9 +11,15 @@ _SD="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 target="${JH_CLOUD_USER}/Systèmes/vps"
 
+mkdir -p "${target}/snapshot/full/"
 rsync --recursive --itemize-changes \
     vps:/var/backups/snapshot/full/ "${target}/snapshot/full/"
 
 jh-backup-take-snapshot.sh "${target}"
 
-# TODO: backup data too...
+mkdir -p "${target}/data/${JH_TIMESTAMP}"
+mkdir -p "${target}/data/latest"
+rsync --recursive --itemize-changes \
+    --backup --backup-dir="${target}/data/${JH_TIMESTAMP}" \
+    --delete \
+    vps:/mnt/data/ "${target}/data/latest"
