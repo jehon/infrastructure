2f6abf618b3f7ae1d2f87bef0a50b34d98721815
Jean Honlet <jehon@users.noreply.github.com>
2024-05-29 10:52:57 +0200

    refactor: use jehon-cloud

--- test.sh
+++ test.sh
@@ -5,12 +5,13 @@ set -o pipefail
 shopt -s nullglob
 
 host=kiosk
-service=mnt-cloud-musiques.mount
+service=jehon-cloud@Musiques.service
 
 ./deploy-patch-from-packages "${host}" \
     packages/jehon/usr/lib/systemd/system/jehon-update-rclone.service \
     packages/jehon/usr/lib/systemd/system/jehon-update-rclone.timer \
-    packages/jehon/usr/lib/systemd/system/mnt-cloud-musiques.mount
+    packages/jehon/usr/lib/systemd/system/jehon-cloud@.service \
+    packages/jehon/usr/sbin/jehon-cloud-mount
 
 # ./deploy-patch-from-packages "${host}" \
 #     packages/jehon-service-backend/usr/share/jehon-service-backend/docker-compose.yml
@@ -19,44 +20,54 @@ service=mnt-cloud-musiques.mount
 ssh "${host}" <<EOS
 clear
 systemctl daemon-reload
+# systemctl disable mnt-cloud-musiques.mount
+# systemctl disable jehon-update-rclone.service
+# systemctl disable jehon-update-rclone.timer
+# systemctl enable jehon-cloud@Musiques.service
 
 echo "Service: ${service}"
 
-# echo "..."; sleep 1
-# systemctl restart "${service}"
+echo "..."; sleep 1
+systemctl restart "${service}"
 
-# echo "..."; sleep 5
-# systemctl status "${service}"
-# journalctl -u "${service}" -n 60
-
-systemctl enable mnt-cloud-musiques.mount
-systemctl disable jehon-update-rclone.service
-systemctl disable jehon-update-rclone.timer
-
-echo "..."; sleep 2
-
-# Stats
-echo "---------- Hardware Stats ------------------"
-top -bn1 | head -n 5
+echo "..."; sleep 5
+systemctl status "${service}"
+journalctl -u "${service}" -n 60
 
 EOS
 
-# This will always fail
-ssh "${host}" reboot || true
+# # This will always fail
+# ssh "${host}" reboot || true
 
-sleep 20s
-while true; do
-    clear
-    echo -n "Testing... "
-    ping kiosk -c 1
-    ssh kiosk ls /
-    sleep 5s
-done
+# sleep 20s
+# while true; do
+#     ping kiosk -c 1 && break || true
+# done
+# echo "** Ping ok"
+
+# while true; do
+#     ssh kiosk "ls /" && break || true
+#     sleep 5s
+# done
+# echo "** SSH ok"
+
+# # shellcheck disable=SC2087 # client side expension
+# ssh kiosk <<EOS
+
+# systemctl status systemd-user-sessions.service
+# systemctl status "${service}"
+
+# EOS
+
+echo "End of script"
 
 # 1 mount
 # 2 service
 # 3 timer
+# 4 service mount
 
 # 123
 # DEE: ok
-# EDD:
+#
+# EDD: ko
+# DDDE:
