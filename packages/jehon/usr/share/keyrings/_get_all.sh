d06eaee1a309f883b702bd8469e97b04d4ab4524
Jean Honlet <jehon@users.noreply.github.com>
2024-06-16 19:38:11 +0200

    feat: add oracle virtualbox gpg key

--- packages/jehon/usr/share/keyrings/_get_all.sh
+++ packages/jehon/usr/share/keyrings/_get_all.sh
@@ -6,7 +6,7 @@ shopt -s nullglob
 
 _SD="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-getGPGKeyRaw() {
+getGPGKey() {
     TARGET="$1"
     URL="$2"
 
@@ -16,7 +16,7 @@ getGPGKeyRaw() {
     echo "Saving to ${TARGET}"
 }
 
-getGPGKeyArmored() {
+getGPGKeyAndDearmor() {
     TARGET="$1"
     URL="$2"
 
@@ -26,7 +26,8 @@ getGPGKeyArmored() {
     echo "Saving to ${TARGET}"
 }
 
-getGPGKeyArmored "docker-ubuntu" "https://download.docker.com/linux/ubuntu/gpg"
-getGPGKeyRaw "github-cli" "https://cli.github.com/packages/githubcli-archive-keyring.gpg"
-getGPGKeyArmored "hashicorp" "https://apt.releases.hashicorp.com/gpg"
-getGPGKeyArmored "node" "https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key"
+getGPGKeyAndDearmor "docker-ubuntu" "https://download.docker.com/linux/ubuntu/gpg"
+getGPGKey "github-cli" "https://cli.github.com/packages/githubcli-archive-keyring.gpg"
+getGPGKeyAndDearmor "hashicorp" "https://apt.releases.hashicorp.com/gpg"
+getGPGKeyAndDearmor "node" "https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key"
+getGPGKeyAndDearmor "virtualbox" "https://www.virtualbox.org/download/oracle_vbox_2016.asc"
