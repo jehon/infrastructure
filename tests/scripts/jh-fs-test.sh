2aadde5c2404fa9b101085c037fa5aa286d2a8cd
Jean Honlet <jehon@users.noreply.github.com>
2024-06-16 10:05:31 +0200

    feat: add file-to-path

--- tests/scripts/jh-fs-test.sh
+++ tests/scripts/jh-fs-test.sh
@@ -3,23 +3,25 @@
 # shellcheck source-path=SCRIPTDIR
 . "$(dirname "${BASH_SOURCE[0]}")/../test-helpers.sh"
 
-SCRIPT="$JH_PKG_FOLDER"/packages/jehon/usr/bin/jh-fs
+script="$JH_PKG_FOLDER"/packages/jehon/usr/bin/jh-fs
 
-assert_equals "name" "name"    "$($SCRIPT "name" "/etc/name.conf")"
-assert_equals "name" "another" "$($SCRIPT "name" "/etc/another")"
-assert_equals "name" "etc"     "$($SCRIPT "name" "/etc/")"
-assert_equals "name" "conf"    "$($SCRIPT "name" "/etc/conf")"
+assert_equals "name" "name" "$("${script}" "name" "/etc/name.conf")"
+assert_equals "name" "another" "$("${script}" "name" "/etc/another")"
+assert_equals "name" "etc" "$("${script}" "name" "/etc/")"
+assert_equals "name" "conf" "$("${script}" "name" "/etc/conf")"
 
-assert_equals "extension" "ext" "$($SCRIPT "extension" "/etc/conf.ext")"
-assert_equals "extension" ""    "$($SCRIPT "extension" "/etc/conf")"
-assert_equals "extension" ""    "$($SCRIPT "extension" "/etc/")"
+assert_equals "extension" "ext" "$("${script}" "extension" "/etc/conf.ext")"
+assert_equals "extension" "" "$("${script}" "extension" "/etc/conf")"
+assert_equals "extension" "" "$("${script}" "extension" "/etc/")"
 
-assert_equals "to-extension" "/etc/conf.test" "$($SCRIPT "to-extension" "/etc/conf.conf" ".test")"
-assert_equals "to-extension" "/etc/conf.test" "$($SCRIPT "to-extension" "/etc/conf" ".test")"
-assert_equals "to-extension" "/etc.test"      "$($SCRIPT "to-extension" "/etc/" ".test")"
+assert_equals "to-extension" "/etc/conf.test" "$("${script}" "to-extension" "/etc/conf.conf" ".test")"
+assert_equals "to-extension" "/etc/conf.test" "$("${script}" "to-extension" "/etc/conf" ".test")"
+assert_equals "to-extension" "/etc.test" "$("${script}" "to-extension" "/etc/" ".test")"
 
-assert_success "not-empty" "$SCRIPT" "not-empty" "/etc/"
-assert_failure "not-empty" "$SCRIPT" "not-empty" "/etc/anything"
+assert_success "not-empty" "${script}" "not-empty" "/etc/"
+assert_failure "not-empty" "${script}" "not-empty" "/etc/anything"
 
-assert_failure "is-empty" "$SCRIPT" "is-empty" "/etc/"
-assert_success "is-empty" "$SCRIPT" "is-empty" "/etc/anything"
+assert_failure "is-empty" "${script}" "is-empty" "/etc/"
+assert_success "is-empty" "${script}" "is-empty" "/etc/anything"
+
+assert_equals "file-to-path" "/etc/apt/sources.list.d/jehon-hashicorp.sources" "$("${script}" "file-to-path" "jehon/scripts/hooks/etc_apt_sources.list.d_jehon-hashicorp.sources.sh")"
