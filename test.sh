7521bfc4d7ceab83aca2b11cc8e6bf8f654dbdc8
Jean Honlet <jehon@users.noreply.github.com>
2024-06-25 13:24:46 +0200

    fix: jehon-cloud service

--- test.sh
+++ test.sh
@@ -4,68 +4,40 @@ set -o errexit
 set -o pipefail
 shopt -s nullglob
 
-host=kiosk
-service=jehon-cloud@Musiques.service
+. jh-lib
 
-# ./deploy-patch-from-packages "${host}" \
-#     packages/jehon/usr/lib/systemd/system/jehon-update-rclone.service \
-#     packages/jehon/usr/lib/systemd/system/jehon-update-rclone.timer \
-#     packages/jehon/usr/lib/systemd/system/jehon-cloud@.service \
-#     packages/jehon/usr/sbin/jehon-cloud-mount
+host=dev
+service=jehon-cloud@Photos.service
 
-rsync -r --delete packages/jehon-service-eternal/usr/share/jehon-service-eternal/stack/ \
-    ${host}:/usr/share/jehon-service-eternal/stack/
+./deploy-patch-from-packages "${host}" \
+    packages/jehon/usr/lib/systemd/system/jehon-cloud@.service \
+    packages/jehon/usr/sbin/jehon-cloud-mount
 
 # shellcheck disable=SC2087 # client side expension
-ssh "${host}" <<EOS
+# ssh "${host}" <<EOS
 clear
-# systemctl daemon-reload
 
-/usr/sbin/jehon-eternal install
+echo "Service: ${service}"
 
-# echo "Service: ${service}"
+jh_info "Daemon reload"
+sudo systemctl daemon-reload
 
-# echo "..."; sleep 1
-# systemctl restart "${service}"
+echo "..."
+sleep 1
 
-# echo "..."; sleep 5
-# systemctl status "${service}"
-# journalctl -u "${service}" -n 60
+jh_info "Restarting ${service}"
+if ! sudo systemctl restart "${service}"; then
+    sudo journalctl -u "${service}" -n 150 -f
+fi
 
-EOS
+echo "..."
+sleep 5
+jh_info "Status of ${service}"
+sudo systemctl status "${service}" | tee
 
-# # This will always fail
-# ssh "${host}" reboot || true
-
-# sleep 20s
-# while true; do
-#     ping kiosk -c 1 && break || true
-# done
-# echo "** Ping ok"
-
-# while true; do
-#     ssh kiosk "ls /" && break || true
-#     sleep 5s
-# done
-# echo "** SSH ok"
-
-# # shellcheck disable=SC2087 # client side expension
-# ssh kiosk <<EOS
-
-# systemctl status systemd-user-sessions.service
-# systemctl status "${service}"
-
-# EOS
+sudo ls -l /mnt/cloud/
+sudo ls -l /mnt/cloud/photos
+ls -l /mnt/cloud/photos
 
 echo "End of script"
-
-# 1 mount
-# 2 service
-# 3 timer
-# 4 service mount
-
-# 123
-# DEE: ok
-#
-# EDD: ko
-# DDDE:
+# EOS
