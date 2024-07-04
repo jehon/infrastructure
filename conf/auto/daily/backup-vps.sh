cd7bc8064d4680951f2016ac2f5574a8cb84d95e
Jean Honlet <jehon@users.noreply.github.com>
2024-07-04 14:53:22 +0200

    style: add headers

--- conf/auto/daily/backup-vps.sh
+++ conf/auto/daily/backup-vps.sh
@@ -21,11 +21,15 @@ if [ ! -r "${target}" ]; then
     header_end
 fi
 
+header_begin "Full: Syncing data"
 rsync --recursive --itemize-changes \
     vps:/var/backups/snapshot/full/ "${target}/snapshot/full/"
-
+header_end
+header_begin "Full: Taking snapshots"
 jh-backup-take-snapshot.sh "${target}"
+header_end
 
+header_begin "Incremental: Syncing data"
 latestDir="${target}/data/latest"
 backupDir="${target}/data/${JH_TIMESTAMP}"
 mkdir -p "${backupDir}"
@@ -34,5 +38,8 @@ rsync --recursive --itemize-changes \
     --backup --backup-dir="${backupDir}" \
     --delete \
     vps:/mnt/data/ "${latestDir}"
+header_end
 
-find "${target}/data/" -type d -mindepth 1 -empty -delete
+header_begin "Remove empty folders"
+find "${target}/data/" -type d -empty -delete
+header_end
