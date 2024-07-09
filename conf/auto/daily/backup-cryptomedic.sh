c5869ff0c25f7abd6e8a0b33e6d1742ecb3ef835
Jean Honlet <jehon@users.noreply.github.com>
2024-07-09 21:28:37 +0200

    feat: add cryptomedic backup (old server)

---
+++ conf/auto/daily/backup-cryptomedic.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+shopt -s nullglob
+
+_SD="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+# shellcheck source-path=SCRIPTDIR/
+. "${_SD}/../../../bin/lib.sh"
+
+target="$JH_CLOUD_USER"/Systèmes/cryptomedic/backups/
+
+# shellcheck source-path=SCRIPTDIR/../../../
+"${JH_PKG_FOLDER}"/bin/jh-wait-home-cloud "${target}"
+
+# shellcheck source=/dev/null
+. "$HOME"/restricted/cryptomedic.sh
+
+echo "Generating a new backup on remote"
+curl -fsSL "http://${CRYPTOMEDIC_DEPLOY_WEB_HOST}/maintenance/create_db_backup.php?pwd=${CRYPTOMEDIC_DEPLOY_WEB_TOKEN}"
+echo "- Sleeping a bit to allow the file to become visible"
+sleep 10s
+echo "...done"
+
+echo "Getting storage"
+lftp "${CRYPTOMEDIC_DEPLOY_FILES_USER}:${CRYPTOMEDIC_DEPLOY_FILES_PASSWORD=}@${CRYPTOMEDIC_DEPLOY_FILES_HOST}" \
+    -e "mirror live/ '$target/'; bye"
+echo "...done"
+
+ok "Backup finished with success"
