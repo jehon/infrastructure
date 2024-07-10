92566232cb564f42a9449d6fbc4c7b9b89d91079
Jean Honlet <jehon@users.noreply.github.com>
2024-07-10 14:38:25 +0200

    fix: use prjRoot

--- bin/lib.sh
+++ bin/lib.sh
@@ -3,8 +3,8 @@
 set -o errexit
 set -o pipefail
 
-prjFolder="$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")"
-export prjFolder
+prjRoot="$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")"
+export prjRoot
 export prjTmp="$JH_PKG_FOLDER/tmp"
 
 export PATH="${JH_PKG_FOLDER}/tmp/python/common/bin/:${JH_PKG_FOLDER}/packages/jehon/usr/bin/:$PATH"
@@ -12,7 +12,7 @@ export PYTHONPATH="$JH_PKG_FOLDER/tmp/python/common"
 
 # FIXME(legacy): Old notation
 export PRJ_TMP="${prjTmp}"
-export JH_PKG_FOLDER="${prjFolder}"
+export JH_PKG_FOLDER="${prjRoot}"
 
 # shellcheck source-path=SCRIPTDIR/../
 . "${prjFolder}/packages/jehon/usr/bin/jh-lib"
