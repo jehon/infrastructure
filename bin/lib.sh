6d2d33c3c8464ca3aa62e5c3286f76ac2e24fdee
Jean Honlet <jehon@users.noreply.github.com>
2024-07-10 15:58:26 +0200

    fix: some variables in bin/lib

--- bin/lib.sh
+++ bin/lib.sh
@@ -5,16 +5,12 @@ set -o pipefail
 
 prjRoot="$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")"
 export prjRoot
-export prjTmp="$JH_PKG_FOLDER/tmp"
+export prjTmp="$prjRoot/tmp"
 
-export PATH="${JH_PKG_FOLDER}/tmp/python/common/bin/:${JH_PKG_FOLDER}/packages/jehon/usr/bin/:$PATH"
-export PYTHONPATH="$JH_PKG_FOLDER/tmp/python/common"
-
-# FIXME(legacy): Old notation
-export PRJ_TMP="${prjTmp}"
-export JH_PKG_FOLDER="${prjRoot}"
+export PATH="${prjRoot}/tmp/python/common/bin/:${prjRoot}/packages/jehon/usr/bin/:$PATH"
+export PYTHONPATH="$prjRoot/tmp/python/common"
 
 # shellcheck source-path=SCRIPTDIR/../
-. "${prjFolder}/packages/jehon/usr/bin/jh-lib"
+. "${prjRoot}/packages/jehon/usr/bin/jh-lib"
 
 mkdir -p "$prjTmp"
