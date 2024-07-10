e1486748f746d02970e155dba22053f2e8875fad
Jean Honlet <jehon@users.noreply.github.com>
2024-07-10 16:59:45 +0200

    fix: refactor jh-lib-test

--- tests/scripts/jh-lib-test.sh
+++ tests/scripts/jh-lib-test.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 
 set -o errexit
-
 # shellcheck source-path=SCRIPTDIR
 . "$(dirname "${BASH_SOURCE[0]}")/../test-helpers.sh"
 
 # Script Working Directory
 TWD="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
-
-#
-# We need to re-import it for JH_SWD to be set correctly
-#
-# shellcheck source=SCRIPTDIR/../../packages/jehon/usr/bin/jh-lib
-. "$JH_PKG_FOLDER/packages/jehon/usr/bin/jh-lib"
 
 assert_equals "JH_SWD" "$TWD" "$JH_SWD"
 
