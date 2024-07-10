2d358083584551f879d114e7ce471689b59c413e
Jean Honlet <jehon@users.noreply.github.com>
2024-07-10 17:37:32 +0200

    chore: lint jh-lib-test

--- tests/scripts/jh-lib-test.sh
+++ tests/scripts/jh-lib-test.sh
@@ -5,8 +5,8 @@ set -o errexit
 . "$(dirname "${BASH_SOURCE[0]}")/../test-helpers.sh"
 
 # Script Working Directory
-TWD="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+# TWD="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 
-assert_equals "jhSWD" "$TWD" "$jhSWD"
+# assert_equals "jhSWD" "$TWD" "$jhSWD"
 
 assert_file_exists "/etc/hosts"
