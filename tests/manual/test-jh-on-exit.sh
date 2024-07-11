78c2d5af6e4171f8d0cc1a755d2fba30bb6776b7
Jean Honlet <jehon@users.noreply.github.com>
2024-07-11 13:24:02 +0200

    refactor: test

--- tests/manual/test-jh-on-exit.sh
+++ tests/manual/test-jh-on-exit.sh
@@ -7,20 +7,19 @@ prjRoot="$(dirname "$(dirname "$(dirname "${BASH_SOURCE[0]}")")")"
 # shellcheck source=SCRIPTDIR/../../packages/jehon/usr/bin/jh-lib
 . "$prjRoot"/packages/jehon/usr/bin/jh-lib
 
-ee1() {
-    echo "cool, on exit 1"
+ee() {
+    echo "* cool, on exit" "$@"
+    echo "1: $1"
+    echo "2: $2"
 }
-jh_on_exit_success ee1
+jh_on_exit_success ee
+jh_on_exit_success "ee 1 2"
+jh_on_exit_success "ee '1 2'"
 
-jh_on_exit_success "echo 'test'"
-jh_on_exit_success "echo 'test test'"
+jh_on_exit_failure "echo 'it is a failure'"
 
-ee2() {
-    echo "cool, on exit 2"
-}
-jh_on_exit_success ee2
+if [ -n "$1" ]; then
+    jh_fatal "force failing"
+fi
 
-jh_on_exit_failure "echo \"it is a failure\""
-echo "at the end"
-
-# false
+ok
