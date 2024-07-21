ba265031746073a47dfdf9c177e16be69fa7b311
Jean Honlet <jehon@users.noreply.github.com>
2024-07-21 12:51:39 +0200

    fix: some crons

--- infrastructure/roles/host_dev/templates/cryptomedic.sh
+++ infrastructure/roles/host_dev/templates/cryptomedic.sh
@@ -11,7 +11,3 @@ export CRYPTOMEDIC_DEPLOY_WEB_TOKEN="{{jehon.credentials.cryptomedic.deploy_web_
 export CRYPTOMEDIC_DEPLOY_FILES_HOST="{{jehon.credentials.cryptomedic.deploy_files_host}}"
 export CRYPTOMEDIC_DEPLOY_FILES_USER="{{jehon.credentials.cryptomedic.deploy_files_user}}"
 export CRYPTOMEDIC_DEPLOY_FILES_PASSWORD="{{jehon.credentials.cryptomedic.deploy_files_password}}"
-
-if [ -n "$1" ]; then
-    "$@"
-fi
