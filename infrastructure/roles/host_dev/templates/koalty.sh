a8fc2b370ef1795cec27fd1d92bd7d139b25c129
Jean Honlet <jehon@users.noreply.github.com>
2024-09-25 09:00:06 +0200

    refactor: flattern configs

--- infrastructure/roles/host_dev/templates/koalty.sh
+++ infrastructure/roles/host_dev/templates/koalty.sh
@@ -4,9 +4,9 @@
 
 set -o errexit
 
-export KOALTY_FTP_HOST="{{jehon.credentials.koalty.ftp.host}}"
-export KOALTY_FTP_USER="{{jehon.credentials.koalty.ftp.user}}"
-export KOALTY_FTP_PASS="{{jehon.credentials.koalty.ftp.pass}}"
+export KOALTY_FTP_HOST="{{jehon_remote_koalty_ftp_host}}"
+export KOALTY_FTP_USER="{{jehon_remote_koalty_ftp_user}}"
+export KOALTY_FTP_PASS="{{jehon_remote_koalty_ftp_pass}}"
 
 if [ -n "$1" ]; then
     "$@"
