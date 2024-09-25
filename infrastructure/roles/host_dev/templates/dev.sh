f877ed5ad55ee9069e4e3fe97e95ab1028ec3dac
Jean Honlet <jehon@users.noreply.github.com>
2024-09-25 13:26:47 +0200

    refactor: flattern config

--- infrastructure/roles/host_dev/templates/dev.sh
+++ infrastructure/roles/host_dev/templates/dev.sh
@@ -6,7 +6,7 @@ set -o errexit
 
 export JH_DOCKER_HUB_TOKEN="{{ jehon_remote_docker_token }}"
 
-export JH_PASS_PCLOUD_RO_CLIENT_ID="{{ jehon_remote_pcloud.ro.client_id }}"
-export JH_PASS_PCLOUD_RO_CLIENT_SECRET="{{ jehon_remote_pcloud.ro.client_secret }}"
-export JH_PASS_PCLOUD_RW_CLIENT_ID="{{ jehon_remote_pcloud.rw.client_id }}"
-export JH_PASS_PCLOUD_RW_CLIENT_SECRET="{{ jehon_remote_pcloud.rw.client_secret }}"
+export JH_PASS_PCLOUD_RO_CLIENT_ID="{{ jehon_remote_pcloud_ro_client_id }}"
+export JH_PASS_PCLOUD_RO_CLIENT_SECRET="{{ jehon_remote_pcloud_ro_client_secret }}"
+export JH_PASS_PCLOUD_RW_CLIENT_ID="{{ jehon_remote_pcloud_rw_client_id }}"
+export JH_PASS_PCLOUD_RW_CLIENT_SECRET="{{ jehon_remote_pcloud_rw_client_secret }}"
