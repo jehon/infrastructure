78fe0bd44dbb712d252b5fbd0ede39c2cf68f555
Jean Honlet <jehon@users.noreply.github.com>
2024-09-27 18:14:52 +0200

    fix: adapt p2p sync script

--- conf/hourly/50-p2p-sync.sh
+++ conf/hourly/50-p2p-sync.sh
@@ -11,10 +11,16 @@ set -o errexit
 #
 
 vps_ssh="vps"
-vps_watch="/srv/p2p/watch/"
-vps_downloaded="/srv/p2p/ready/"
+vps_ssh_user="jehon-daemon"
+vps_root="/home/jehon-daemon/stack/volumes/"
+vps_watch="${vps_root}/torrents.var.watched/"
+vps_downloaded="${vps_root}/torrents.var.ready/"
 
 cloud_downloaded="${jhCloudFolderInUserHome}/Workspaces/Jean/Work/p2p/"
+
+vps_ops=(
+    -e "ssh -l $vps_ssh_user"
+)
 
 ##################################
 #
@@ -36,19 +42,21 @@ user_report_failure
 
 header_begin "Uploading desktop files to $vps_watch"
 rsync "${jhRsyncOptions[@]}" \
+    "${vps_ops[@]}" \
     --recursive \
     --remove-source-files \
     --include='*.torrent' --include='*.torrent.magnet' --exclude '*' \
-    "$HOME/Desktop/" "$vps_ssh:$vps_watch"
+    "$HOME/Desktop/" "${vps_ssh}:${vps_watch}"
 header_end
 
 header_begin "Downloading $vps_downloaded to $cloud_downloaded"
 rsync "${jhRsyncOptions[@]}" \
+    "${vps_ops[@]}" \
     --recursive \
     --remove-source-files \
-    "$vps_ssh:$vps_downloaded" "$cloud_downloaded"
+    "${vps_ssh}:${vps_downloaded}" "$cloud_downloaded"
 header_end
 
 header_begin "Cleaning remote"
-ssh vps find "$vps_downloaded" -type d -mindepth 1 -empty -delete
+ssh "$vps_ssh_user@$vps_ssh" find "$vps_downloaded" -type d -mindepth 1 -empty -delete
 header_end
