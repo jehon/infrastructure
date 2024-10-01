1f5c339b10d7cf8a7950d978c00d92ebe5b2c7e2
Jean Honlet <jehon@users.noreply.github.com>
2024-10-01 14:36:27 +0200

    feat: p2p upload is from vps directly

--- conf/hourly/50-p2p-sync.sh
+++ conf/hourly/50-p2p-sync.sh
@@ -48,15 +48,3 @@ rsync "${jhRsyncOptions[@]}" \
     --include='*.torrent' --include='*.torrent.magnet' --exclude '*' \
     "$HOME/Desktop/" "${vps_ssh}:${vps_watch}"
 header_end
-
-header_begin "Downloading $vps_downloaded to $cloud_downloaded"
-rsync "${jhRsyncOptions[@]}" \
-    "${vps_ops[@]}" \
-    --recursive \
-    --remove-source-files \
-    "${vps_ssh}:${vps_downloaded}" "$cloud_downloaded"
-header_end
-
-header_begin "Cleaning remote"
-ssh "$vps_ssh_user@$vps_ssh" find "$vps_downloaded" -type d -mindepth 1 -empty -delete
-header_end
