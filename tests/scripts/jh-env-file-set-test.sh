463a9b2d89282797320c1c7104ae22da4cd77fc5
Jean Honlet <jehon@users.noreply.github.com>
2024-10-09 13:13:31 +0200

    feat: jh-env-file-set

---
+++ tests/scripts/jh-env-file-set-test.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# shellcheck source-path=SCRIPTDIR
+. "$(dirname "${BASH_SOURCE[0]}")/../test-helpers.sh"
+
+script="$prjRoot"/packages/jehon/usr/bin/jh-env-file-set
+
+# JH_TEST_TMP
+
+declare test
+declare test2
+
+e1="$JH_TEST_TMP/test.env"
+rm -f "$e1"
+
+assert_success "init" "$script" "$e1" "test" "cool"
+(
+    # shellcheck source=/dev/null
+    . "$e1"
+
+    assert_equals "test" "cool" "$test"
+)
+
+assert_success "update" "$script" "$e1" "test" "cool1"
+(
+    # shellcheck source=/dev/null
+    . "$e1"
+    assert_equals "test" "cool1" "$test"
+)
+
+assert_success "add" "$script" "$e1" "test2" "cool2"
+(
+    # shellcheck source=/dev/null
+    . "$e1"
+    assert_equals "test" "cool1" "$test"
+    assert_equals "test2" "cool2" "$test2"
+)
