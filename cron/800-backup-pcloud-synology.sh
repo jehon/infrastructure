5b6a05e840e2a3c4aa4f10a4d5656f2db5aad3a9
Jean Honlet <jehon@users.noreply.github.com>
2024-12-26 08:56:16 +0100

    refactor: auto scripts

--- cron/800-backup-pcloud-synology.sh
+++ cron/800-backup-pcloud-synology.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o pipefail
 
 # shellcheck source-path=SCRIPTDIR/
-. "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../bin/conf-lib.sh"
+. "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../bin/conf-lib.sh"
 
 ##################################
 #
