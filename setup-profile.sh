ab6d9763b1d97d77530f4b6477802db420f1bfa2
Jean Honlet <jehon@users.noreply.github.com>
2025-01-07 08:53:52 +0100

    refactor: migrate to scripts

--- setup-profile.sh
+++ setup-profile.sh
@@ -19,10 +19,4 @@ if [ -n "$SWD" ]; then
     LOCAL_BIN="${SWD}/packages/jehon/usr/bin"
     echo "Path: Adding '$LOCAL_BIN'"
     export PATH="$LOCAL_BIN:$PATH"
-
-    LOCAL_BIN="${SWD}/bin"
-    echo "Path: Adding '$LOCAL_BIN'"
-    export PATH="$LOCAL_BIN:$PATH"
 fi
-
-export RCLONE_CONFIG=/etc/jehon/rclone.conf
